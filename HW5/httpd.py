280fa858c663ef803580b8da3c86e3205f04dec0
Виталий Сотников <yozhkin.code@gmail.com>
2018-01-01 16:44:23 +0500

    Add error handling and logging

--- HW5/httpd.py
+++ HW5/httpd.py
@@ -1,8 +1,9 @@
+import logging
 import socket
 from concurrent.futures import ThreadPoolExecutor
 from optparse import OptionParser
 
-from constants import REQUEST_URI_TOO_LONG, http_code_to_description, MAX_URI_LENGTH, CHUNK_SIZE
+from constants import REQUEST_URI_TOO_LONG, http_code_to_description, MAX_URI_LENGTH, CHUNK_SIZE, INTERNAL_SERVER_ERROR
 from request import Request
 from response import Response
 
@@ -42,23 +43,38 @@ def handle_client_connection(client_sock: socket.socket, document_root):
     response = Response()
     response.build_server_headers()
 
-    body, error_code = socket_reader(client_sock)
-    if error_code:
-        status_description = http_code_to_description[error_code]
-        response.set_status_line(error_code, status_description)
-        response.prepare()
-    else:
-        request = Request(body)
-        method, path = request.parse_request()
-        http_code, status_description, fd = response.to_serve_request(method, path, document_root)
-        response.set_status_line(http_code, status_description)
-        response.set_additional_headers(fd, method, path)
-        response.prepare()
-        if fd and fd.content:
-            response.response += fd.content
+    try:
+        body, error_code = socket_reader(client_sock)
+        if error_code:
+            status_description = http_code_to_description[error_code]
+            response.set_status_line(error_code, status_description)
+            response.prepare()
+        else:
+            request = Request(body)
+            method, path = request.parse_request()
+            http_code, status_description, fd = response.to_serve_request(method, path, document_root)
+            response.set_status_line(http_code, status_description)
+            response.set_additional_headers(fd, method, path)
+            response.prepare()
+            if fd and fd.content:
+                response.response += fd.content
 
-    write_to_socket(client_sock, response.content)
-    client_sock.close()
+        write_to_socket(client_sock, response.content)
+        client_sock.close()
+    except Exception as e:
+        logging.exception(e, exc_info=True)
+        error_code = INTERNAL_SERVER_ERROR
+        status_description = http_code_to_description[error_code]
+
+        message = b'<h1>Ooops, something going wrong!</h1>Please call 911 and wait for help.'
+
+        response.set_status_line(error_code, status_description)
+        response.set_header({'Content-Type': 'text/html'})
+        response.set_header({'Content-Length': len(message)})
+        response.prepare()
+        response.response += message
+        write_to_socket(client_sock, response.content)
+        client_sock.close()
 
 
 def main(address, port, total_workers, document_root):
@@ -67,13 +83,15 @@ def main(address, port, total_workers, document_root):
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.bind((address, port))
     s.listen(5)
+    logging.info('Server was started')
     print("\nPress Ctrl+C to shut down server.")
     while True:
         try:
-            client_sock, _ = s.accept()
+            client_sock, (address, port), = s.accept()
+            logging.info('Connection on {} port'.format(port))
             pool.submit(handle_client_connection, client_sock=client_sock, document_root=document_root)
-        except Exception as e:
-            print(e)
+        except socket.error as e:
+            logging.exception(e, exc_info=True)
 
 
 if __name__ == "__main__":
@@ -84,11 +102,17 @@ if __name__ == "__main__":
     op.add_option("-r", "--document_root", action="store", default='/')
     opts, _ = op.parse_args()
     try:
+        logging.basicConfig(level=logging.INFO,
+                            format='[%(asctime)s] %(levelname).1s %(message)s',
+                            datefmt='%Y.%m.%d %H:%M:%S')
         total_workers = int(opts.workers)
         document_root = opts.document_root
         port = int(opts.port)
         address = opts.address
         main(address, port, total_workers, document_root)
     except KeyboardInterrupt:
+        logging.info('Server was stopped')
         print("\nBye-bye...")
         exit(0)
+    except Exception as e:
+        logging.exception(e, exc_info=True)
